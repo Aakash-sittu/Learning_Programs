2c9808d694a0b1bc31b50835c8024a5e0d05b781
Aakash <93485049+Aakash-sittu@users.noreply.github.com>
2022-07-25 23:37:50 +0530

    Wave Array

---
+++ Wave_Array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
