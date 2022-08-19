cbade6d2b1cdbac76ff0c6e22b8e9dbc4a110c98
Aakash <93485049+Aakash-sittu@users.noreply.github.com>
2022-08-19 23:43:23 +0530

    Bitwise Usage

---
+++ bitwise.cpp
@@ -0,0 +1,16 @@
+// bitwise operator
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 30, b = 12;
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    // cout<<(a!b)<<endl;
+    cout << (!a) << endl;
+    cout << (!b) << endl;
+
+    return 0;
+}
