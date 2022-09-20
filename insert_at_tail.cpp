3a57a00d7bcb61492112bfe1fafb1d0585a21826
Aakash <93485049+Aakash-sittu@users.noreply.github.com>
2022-09-20 08:20:22 +0530

    Insert at Tail in Linked List

---
+++ insert_at_tail.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+void insertattail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void display(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+int main(int argc, char const *argv[])
+{
+    node *head = NULL;
+    insertattail(head, 1);
+    insertattail(head, 2);
+    insertattail(head, 3);
+    display(head);
+    return 0;
+}
